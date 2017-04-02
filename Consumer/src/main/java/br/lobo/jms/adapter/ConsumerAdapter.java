f5b58af6fbfde0ea8e6ff2081e8eec664ee610ff
Felipe Santos <fellipaum@gmail.com>
2017-04-02 14:13:52 -0300

    adapter e junit

---
+++ Consumer/src/main/java/br/lobo/jms/adapter/ConsumerAdapter.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package br.lobo.jms.adapter;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+
+/**
+ * @author Felipe
+ *
+ */
+@Component
+public class ConsumerAdapter {
+	
+	private static Logger logger = LogManager.getLogger(ConsumerAdapter.class.getName());
+
+	public void sendToMongo(String json) {
+		logger.info("Sending to MongoDB");
+		MongoClient client = new MongoClient();
+		MongoDatabase db = client.getDatabase("vendor");
+		MongoCollection<BasicDBObject> collection = db.getCollection("contact",BasicDBObject.class);
+		logger.info("Converting JSON to DBObject");
+		BasicDBObject object = (BasicDBObject) JSON.parse(json);
+		collection.insertOne(object);
+		logger.info(collection.count());
+		logger.info("Sent to MongoDB");
+		client.close();
+	}
+
+}
--- Consumer/src/main/java/br/lobo/jms/listener/ConsumerListener.java
+++ Consumer/src/main/java/br/lobo/jms/listener/ConsumerListener.java
@@ -3,10 +3,18 @@
  */
 package br.lobo.jms.listener;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+
+import br.lobo.jms.adapter.ConsumerAdapter;
 
 /**
  * @author Felipe
@@ -14,12 +22,31 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ConsumerListener implements MessageListener {
+	
+	private static Logger logger = LogManager.getLogger(ConsumerListener.class.getName());
+		
+	@Autowired
+	JmsTemplate jmsTemplate;
+	
+	@Autowired
+	ConsumerAdapter consumerAdapter;
 
 	/* (non-Javadoc)
 	 * @see javax.jms.MessageListener#onMessage(javax.jms.Message)
 	 */
-	public void onMessage(Message arg0) {
-		System.out.println("In onMessage()");
+	public void onMessage(Message message) {
+		logger.info("In onMessage()");
+		String json = null;
+		if (message instanceof TextMessage) {
+			try {
+				json = ((TextMessage) message).getText();
+				logger.info("Sending JSON do DB: " + json);
+				consumerAdapter.sendToMongo(json);
+			} catch (JMSException e) {
+				logger.error("Message: " + json);
+				jmsTemplate.convertAndSend(json);
+			}
+		}
 
 	}
 
